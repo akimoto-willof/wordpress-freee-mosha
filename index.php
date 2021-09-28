2ca089dcee9035cb288c54c9299ba7fde8a14887
秋本 裕之 <hiroyuki.akimoto@willof.co.jp>
2021-09-28 17:13:04 +0900

    [modi]LP全体完成

--- index.php
+++ index.php
@@ -63,14 +63,131 @@
                 <div class="news-sub">ニュース</div>
             </div>
             <ul class="news-detail">
-                <li class="list-item"></li>
+                <?php while (have_posts()): the_post(); ?>
+                    <li class="list-item">
+                        <div class="category"><?php echo the_category( $separator, $parents, $post_id ); ?></div>
+                        <div class="date"><?php the_time('Y-m-d'); ?></div>
+                        <div class="title"><a href="#" class="title-link"><?php the_title(); ?></a></div>
+                    </li>
+                <?php endwhile; ?>
             </ul>
-
+            <div class="news-button">
+                <a class="button-detail" href="#">ニュース 一覧</a>
+            </div>
         </section>
 
+        <section class="service">
+            <div class="movie-container">
+                <video src="<?php echo get_template_directory_uri(); ?>/video/movie-top.mp4" class="movie" autoplay loop playsinline muted></video>
+            </div>
+            <div class="comment-container">
+                <div class="movie-title">SERVICES</div>
+                <div class="movie-sub">サービス</div>
+            </div>
+            <ul class="item-list">
+                <li class="list-detail">
+                    <div class="logo-item"><img src="<?php echo get_template_directory_uri(); ?>/images/img-service-kaikei_202106.svg" alt=""></div>
+                    <div class="detail">個人事業主から500名規模の法人まで経理をまとめて効率化</div>
+                </li>
+                <li class="list-detail">
+                    <div class="logo-item"><img src="<?php echo get_template_directory_uri(); ?>/images/img-service-jinji_202106.svg" alt=""></div>
+                    <div class="detail">経営に必要なヒトの情報を集約。人事労務に関する業務を一気通貫するHRプラットフォームサービス</div>
+                </li>
+                <li class="list-detail">
+                    <div class="logo-item"><img src="<?php echo get_template_directory_uri(); ?>/images/img-service-kaishafreee_202106.svg" alt=""></div>
+                    <div class="detail">5分でできる最も簡単な会社設立2,000社以上の会社設立実績</div>
+                </li>
+            </ul>
+            <div class="service-button">
+                <a class="button-detail" href="#">サービス 一覧</a>
+            </div>
+        </section>
+
+        <section class="etc">
+            <ul class="etc-wrapper">
+                <li class="detail">
+                    <div class="primary">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/img-alliance.jpg" alt="" class="etc-img">
+                    </div>
+                    <div class="common">
+                        <div class="title">PARTNER</div>
+                        <div class="sub">パートナー</div>
+                        <p class="item-area">freeeとの連携で、ビジネスがもっとカンタンに。freeeでは、様々な形でアライアンスを組む、パートナーを募集しています。</p>
+                        <button class="etc-button"><a href="" class="button-detail">パートナー</a></button>
+                    </div>
+                </li>
+                <li class="detail">
+                    <div class="primary">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/img-recruit.jpg" alt="" class="etc-img">
+                    </div>
+                    <div class="common">
+                        <div class="title">採用情報</div>
+                        <p class="item-area">freeeは新しいメンバーを募集しています。「小さいビジネスほど強くてかっこいい」そんな社会をいっしょにつくりましょう。</p>
+                        <button class="etc-button"><a href="" class="button-detail">採用ページ</a></button>
+                    </div>
+                </li>
+                <li class="detail">
+                    <div class="primary">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/img-brand.png" alt="" class="etc-img">
+                    </div>
+                    <div class="common">
+                        <div class="title">ブランド</div>
+                        <p class="item-area">freeeは2021年に新たなビジョンを据えて、ブランドを刷新しました。スモールビジネスをさらに世界の主役にするために、freeeは変わり続けます。</p>
+                        <button class="etc-button"><a href="" class="button-detail">ブランドサイト</a></button>
+                    </div>
+                </li>
+            </ul>
+        </section>
     </main>
     <footer>
-
+        <div class="footer">
+            <div class="top-bg"><img src="<?php echo get_template_directory_uri(); ?>/images/bg-footer.jpeg" alt=""></div>
+            <div class="link-wrap">
+                <div class="footer-links">
+                    <dl class="link-list">
+                        <dt class="link-title">会社情報</dt>
+                        <dd>会社概要</dd>
+                        <dd>ミッション</dd>
+                        <dd>ブランド</dd>
+                        <dd>ニュース</dd>
+                        <dd>サービス</dd>
+                        <dd>IR</dd>
+                        <dd>採用情報</dd>
+                        <dd>パートナー</dd>
+                    </dl>
+                    <dl class="link-list">
+                        <dt class="link-title">サービス</dt>
+                        <dd>freee会計</dd>
+                        <dd>freee人事労務</dd>
+                        <dd>freee会社設立</dd>
+                        <dd>freee開業</dd>
+                        <dd>freeeマイナンバー管理</dd>
+                        <dd>freee申告</dd>
+                        <dd>freeeカード</dd>
+                        <dd>freee 開業応援パック</dd>
+                        <dd>freee Developers Community</dd>
+                    </dl>
+                    <dl class="link-list">
+                        <dt class="link-title">メディア・ブログ</dt>
+                        <dd>公式ブログ</dd>
+                        <dd>採用ブログ</dd>
+                        <dd>佐々木大輔のブログ</dd>
+                        <dd>freee Developers Blog</dd>
+                        <dd>経営ハッカー powered by freee</dd>
+                    </dl>
+                    <dl class="link-list">
+                        <dt class="link-title"></dt>
+                        <dd>プライバシーポリシー</dd>
+                        <dd>電子決済等代行業</dd>
+                        <dd>ソーシャルメディア公式アカウント</dd>
+                    </dl>
+                </div>
+                <div class="footer-logo">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/img-site-logo_white_202106.svg" alt="">
+                    <small>Copyright 2021 freee K.K.</small>
+                </div>
+            </div>
+        </div>
     </footer>
     
 </body>
